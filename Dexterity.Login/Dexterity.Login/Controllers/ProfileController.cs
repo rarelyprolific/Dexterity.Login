88655c7245027426ae3cc6297d4fabd6b46e5508
Alan Gryniewicz <alan.gryniewicz@gmail.com>
2019-06-08 22:39:27 +0100

    Added profile page to secure

---
+++ Dexterity.Login/Dexterity.Login/Controllers/ProfileController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dexterity.Login.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
